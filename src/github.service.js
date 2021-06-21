7de7ab6b0a35624021371e583c90512d486edd2e
daniil.egorov <daniil.egorov@spbpu.com>
2021-06-21 17:26:24 +0300

    pushing data to repo

--- src/github.service.js
+++ src/github.service.js
@@ -1,5 +1,31 @@
-export { push }
+import path from 'path'
+import simpleGit from 'simple-git'
+
+const fs = require('fs')
+
+export { push, init }
+
+let git
+let baseDir
 
 function push(filename, data) {
-  console.log(filename, data)
+  const fullname = path.resolve(baseDir, filename)
+  fs.writeFileSync(fullname, data)
+  git.add(fullname)
+  git.commit(`updated ${filename}, ${new Date().toString()}`)
+  git.push()
+}
+
+function init() {
+  baseDir = path.resolve(__dirname, 'sitemaps')
+
+  if (!fs.existsSync(baseDir)) {
+    fs.mkdirSync(baseDir)
+  }
+
+  git = simpleGit(baseDir)
+  git.clone(
+    'https://pricelessnothing:uduvuresy7556@github.com/pricelessnothing/test_actual_repo.git',
+    baseDir,
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const http = require('http')
+const { buildProjectMaps } = require('./app.service')
+const { init } = require('./github.service')
+
+init()
 
 http
-  .createServer((req, res) => {
+  .createServer(async (req, res) => {
     switch (req.url) {
       case '/projects':
+        try {
+          await buildProjectMaps()
+          res.end()
+        } catch (ex) {
+          res.statusCode = ex.status || 500
+          res.end()
+        }
         break
       default:
         res.statusCode = 404
